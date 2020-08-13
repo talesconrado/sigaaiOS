de9986f34082aaac58ff254e69ed6da29905246b
Tales Conrado <talesmateuscc@gmail.com>
2020-08-13 10:05:17 -0300

    Adds gitignore

---
+++ SIGAA-UFC-MobileTests/SIGAA_UFC_MobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  SIGAA_UFC_MobileTests.swift
+//  SIGAA-UFC-MobileTests
+//
+//  Created by Tales Conrado on 11/08/20.
+//  Copyright © 2020 Tales Conrado. All rights reserved.
+//
+
+import XCTest
+@testable import SIGAA_UFC_Mobile
+
+class SIGAA_UFC_MobileTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
